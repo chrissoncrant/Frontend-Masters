e0e11f29031061a017ffcdd900d317be68337a9c
Christopher Soncrant <christopher.soncrant@gmail.com>
2022-12-06 14:33:12 -0800

    created functionality for correct answers and loading next question

--- courses/javascript/first-to-pro/js-quiz/script.js
+++ courses/javascript/first-to-pro/js-quiz/script.js
@@ -1,19 +1,72 @@
 const questions = [
     {
-        question: "How many are there?",
-        options: [1, 2, 3, 4],
-        correct: 1
-    }
+        question: `"1" + "1" === "2"`,
+        options: [true, false],
+        correct: "true",
+        explanation: "This is wrong because..."
+    },
+    {
+        question: `What kind of operator is this: "="?`,
+        options: ['ternary', 'logical', 'assignment', 'arithmetic'],
+        correct: 'assignment'
+    },
 ];
+
+const score = {
+    correct: 0,
+    incorrect: 0
+};
 
 const questionDiv = document.querySelector(".question");
 
 const optionsDiv = document.querySelector(".options");
 
-questionDiv.textContent = questions[0].question;
+let questionNumber = 0;
 
-questions[0].options.forEach(opt => {
-    const button = document.createElement('button');
-    button.textContent = opt;
-    optionsDiv.appendChild(button);
-})
+function loadQuestion(questionNumber) {
+    const currentQuestion = questions[questionNumber];
+
+    questionDiv.textContent = currentQuestion.question;
+
+    const answers = currentQuestion.options;
+
+    buttonClickCount = 0;
+    
+    answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.textContent = answer;
+        button.addEventListener('click', () => {
+            if (button.textContent === currentQuestion.correct) {
+                button.style.backgroundColor = 'var(--correct)';
+                buttonClickCount === 0 && showNextQuestionButton();
+                buttonClickCount++;
+            } else {
+                button.style.backgroundColor = 'var(--incorrect)';
+            }
+        })
+        optionsDiv.appendChild(button);
+    })
+}
+
+function showNextQuestionButton() {
+    const nextBtn = document.createElement('button');
+    nextBtn.textContent = "Next >"
+    nextBtn.classList.add('next');
+    optionsDiv.appendChild(nextBtn);
+
+    let options = optionsDiv.children;
+    
+    nextBtn.addEventListener('click', () => {
+
+        while (options.length > 0) {
+            let child = options[0];
+            optionsDiv.removeChild(child);
+        }
+
+        questionNumber++;
+        
+        loadQuestion(questionNumber);
+    })
+}
+
+loadQuestion(0);
