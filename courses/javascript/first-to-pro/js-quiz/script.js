a4c689a0416a9f974d81302851257b28237cc1e9
Christopher Soncrant <christopher.soncrant@gmail.com>
2022-12-14 18:49:17 -0800

    updated js code as per course. Added new function for checking answer

--- courses/javascript/first-to-pro/js-quiz/script.js
+++ courses/javascript/first-to-pro/js-quiz/script.js
@@ -42,30 +42,49 @@ function enable(button) {
 }
 
 function isCorrect(guess) {
-    return guess === fact.answer.toString() ? "Correct!" : "Wrong...";
+    return guess === fact.answer.toString();
 }
 
 //This variable needs to be globally scoped; it is a state variable. State variable are generally going to be globally scoped. 
-let capitalizeToggle = 1;
+// let capitalizeToggle = 1;
 
-optionsButtons.forEach(button => button.addEventListener('click', (e) => {
-    if (e.target === optionsButtons[0]) {
-        if (capitalizeToggle) {
-            e.target.textContent = e.target.textContent[0].toUpperCase() + e.target.textContent.slice(1);
-            capitalizeToggle--;
-        } else {
-            e.target.textContent = e.target.textContent[0].toLowerCase() + e.target.textContent.slice(1);
-            capitalizeToggle++;
+// optionsButtons.forEach(button => button.addEventListener('click', (e) => {
+//     if (e.target === optionsButtons[0]) {
+//         if (capitalizeToggle) {
+//             e.target.textContent = e.target.textContent[0].toUpperCase() + e.target.textContent.slice(1);
+//             capitalizeToggle--;
+//         } else {
+//             e.target.textContent = e.target.textContent[0].toLowerCase() + e.target.textContent.slice(1);
+//             capitalizeToggle++;
+//         }
+//     }
+//     console.log(isCorrect(e.target.value));
+// }));
+
+
+for (let option of optionsButtons) {
+    option.addEventListener('click', () => {
+        explanation.textContent = fact.explanation;
+        
+        for (let i = 0; i < optionsButtons.length; i++) {
+            disable(optionsButtons[i]);
         }
-    }
-    console.log(isCorrect(e.target.value))
-}));
+        
+        let guess = option.value;
+        
+        if (isCorrect(guess)) {
+            option.classList.add('correct');
+        } else {
+            option.classList.add('incorrect');
+        }
+    })
+}
+
 
 const h1 = document.querySelector('h1');
 
 h1.addEventListener('mouseenter', () => {
     h1.textContent = 'hovering';
-    console.log('yo')
 })
 
 h1.addEventListener('mouseout', () => {
