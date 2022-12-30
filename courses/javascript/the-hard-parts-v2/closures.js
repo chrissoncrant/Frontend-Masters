6c2b3f4d9ed080b0430ef8feae020bf968dc8661
Christopher Soncrant <christopher.soncrant@gmail.com>
2022-12-30 15:38:17 -0800

    updated the closures.js file.

--- courses/javascript/the-hard-parts-v2/closures.js
+++ courses/javascript/the-hard-parts-v2/closures.js
@@ -1,8 +1,86 @@
-//Section 2:
+//Section 3: Closure Challenges
+// http://csbin.io/closures
+
+
+
+//Section 2: 
+function outer() {
+    let counter = 0;
+
+    function incrementCounter() {
+        counter++;
+    }
+
+    incrementCounter();
+
+    // console.log(counter);
+}
+
+outer();
+outer();
+outer();
+
+//No matter how many times outer is called, the counter variable will only ever reach 1. This is because with each call the counter variable is created and set to 0. 
+
+//In order to have this counter's data to persist we can use closures:
+
+function outerWithClosure() {
+    let counter = 0;
+
+    // function incrementCounter() {
+    //     counter++;
+
+    //     // console.log(counter);
+    // }
+
+    return () => {
+        counter++;
+
+        // console.log(counter);
+    };
+}
+
+const incrementCounter = outerWithClosure();
+
+incrementCounter();
+incrementCounter();
+incrementCounter();
+//Now, with the above, we have a counter that will increment and persist the counter variable's data as it changes. Note that the counter variable is not globally accessible.
+
+
+//Below is a new instance of the same closure above. This does not interfere with the first instance. It starts fresh, it gets a new backpack.
+const incrementCounter2 = outerWithClosure();
+
+incrementCounter2();
+incrementCounter2();
+incrementCounter2();
+
+//Below is connecting the identifier incrementCounter3 with the value (which is a closure) that is connected to incrementCounter. In this case calling incrementCounter3 will update the counter variable that was already updated by calling incrementCounter previousely.
+const incrementCounter3 = incrementCounter;
+
+incrementCounter3();
+incrementCounter3();
+
+
+//What we are doing below is showing that the running/calling of a function doesn't determine the access to variables, but rather it is the environment within which the funciton is defined that matters.
+function incrementCounter4() {
+    counter++;
+    console.log(counter);
+}
+
+function outer3() {
+    let counter = 0;
+
+    incrementCounter4();
+}
+
+// outer3();
+
+
 // ######################################################
 // ######################################################
 
-//Section 1:
+//Section 1: Intro to Closures
 function createFunction() {
     //This variable is locally scoped, yet the funciton definition whose identifier within this scope is multiplyBy2 will have access to it and will be able to manipulate it and retrieve it via its identifier. 
     let count = 0;
@@ -34,7 +112,7 @@ function createFunction() {
 }
 
 //This constant is now the identifier for the function definition of what was multiplyBy2. 
-const generatedFunction = createFunction();
+// const generatedFunction = createFunction();
 
 //Doing the above is similar if not the same thing as doing this:
 // const generatedFunction = function(num) {
@@ -42,14 +120,14 @@ const generatedFunction = createFunction();
 // }
 
 //This constant's identifier is now assigned the value returned by generatedFunction.
-const result = generatedFunction(4);
+// const result = generatedFunction(4);
 
-console.log(`Result 1: ${result}`);
+// console.log(`Result 1: ${result}`);
 
 //This constant's identifier is tied to a different value due to the variable 'count'. It is tied to a new function definition that was defined within the multiplyBy2 function that was defined within the createFunction call. It is a grandbaby function definition, yet it still has access to its parent's and it's grandparent's stored values. 
-const result2 = generatedFunction(6);
-console.log(`Result 2: ${result2()}`);
+// const result2 = generatedFunction(6);
+// console.log(`Result 2: ${result2()}`);
 
 //Every time this function is called it will update the count variable. 
-generatedFunction(2);
-console.log(`Result 3: ${result2()}`);
+// generatedFunction(2);
+// console.log(`Result 3: ${result2()}`);
